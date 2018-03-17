949f198b0441149d32657e31afb49666ae04b847
Megan Aldridge <m.aldridge@live.com>
2018-03-17 15:49:44 -0400

    Added manual testing functions

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,6 +1,8 @@
 
 
+import java.util.*;
 import junit.framework.TestCase;
+
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -18,11 +20,83 @@ public class UrlValidatorTest extends TestCase {
    }
 
    
+   public void testManualTest1() {
+	   /* 
+	    * Assert 
+	    * http://www.google.com 
+	    * is valid */
+	   String[] schemes = {"http", "https"};
+       UrlValidator urlValidator = new UrlValidator(schemes);
+	   String url = "http://www.google.com";
+	   boolean isValid = true;  
+	   assertEquals(url, isValid, urlValidator.isValid(url));
+   }
    
-   public void testManualTest()
+   public void testManualTest2() {
+	   /* 
+	    * Assert 
+	    * https://www.google.com 
+	    * is valid 
+	    * */
+	   String[] schemes = {"http", "https"};
+       UrlValidator urlValidator = new UrlValidator(schemes);
+	   String url = "https://www.google.com";
+	   boolean isValid = true;  
+	   assertEquals(url, isValid, urlValidator.isValid(url));
+   }   
+   
+   public void testManualTest3() {
+	   /* 
+	    * Assert 
+	    * https://www.yahoo.com/?err=404&err_url=https%3a%2f%2fwww.yahoo.com%2ftech%2frc%2fdesktops%2f102 
+	    * is valid 
+	    * */ 
+	   String[] schemes = {"http","https"};
+       UrlValidator urlValidator = new UrlValidator(schemes);
+       String url = "https://www.yahoo.com/?err=404&err_url=https%3a%2f%2fwww.yahoo.com%2ftech%2frc%2fdesktops%2f102";
+       boolean isValid = true;
+       assertEquals(url, isValid, urlValidator.isValid(url));
+   }
+   
+   public void testManualTest4() {
+	   /* 
+	    * Assert 
+	    * https://www.yahoo.com/?err=404&err_url=https%3a%2f%2fwww.yahoo.com%2ftech%2frc%2fdesktops%2f102 
+	    * is valid 
+	    * */ 
+	   String[] schemes = {"http","https"};
+       UrlValidator urlValidator = new UrlValidator(schemes);
+       String url = "https://www.yahoo.com/?err=404&err_url=https%3a%2f%2fwww.yahoo.com%2ftech%2frc%2fdesktops%2f102";
+       boolean isValid = true;
+       assertEquals(url, isValid, urlValidator.isValid(url));
+   }
+   
+   public void testManualTest5()
    {
-//You can use this function to implement your manual testing	   
-	   
+	   /* 
+	    * Assert 
+	    * ht://www.google.com 
+	    * is invalid 
+	    * */ 
+	   String[] schemes = {"http","https"};
+       UrlValidator urlValidator = new UrlValidator(schemes);
+       String url = "ht://www.google.com";
+       boolean isValid = false;
+       assertEquals(url, isValid, urlValidator.isValid(url));
+   }
+   
+   public void testManualTest6()
+   {
+	   /* 
+	    * Assert 
+	    * (blank)
+	    * is invalid 
+	    * */ 
+	   String[] schemes = {"http","https"};
+       UrlValidator urlValidator = new UrlValidator(schemes);
+       String url = "";
+       boolean isValid = false;
+       assertEquals(url, isValid, urlValidator.isValid(url));
    }
    
    
